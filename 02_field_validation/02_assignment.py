cf24773c3e41e6b8dd8f865e97b317fddb7df595
Pratik Raj <pratikraj220011@gmail.com>
2025-05-23 00:27:13 +0530

    know how to use field to more type safety

---
+++ 02_field_validation/02_assignment.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+class Employee(BaseModel):
+  id: int
+  name:str = Field(..., min_length=3, max_length=50, )
+  department: Optional[str] = 'General'
+  salary: float = Field(..., ge=10000)
